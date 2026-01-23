b981a6e81fc3ef5f46a8ee45fd0fa810e032e3cb
Justus Kluge <jkluge@kth.se>
2026-01-23 01:56:09 +0100

    Reorganization and 1.3

--- A1/plot2.py
+++ A1/plot2.py
@@ -7,54 +7,63 @@ Created on Thu Jan 22 23:05:03 2026
 """
 
 
-#% Import
+# % Import
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-#%%Plot a
+# %%Plot a
 
-x = np.linspace(-2, 3)     # Creates 100 values in an array from -2 to 3
-y1 = np.exp(-(x)**2)            #
-y2 = np.exp(-((x-2)**2))
+x = np.linspace(-2, 3)  # Creates 100 values in an array from -2 to 3
+y1 = np.exp(-((x) ** 2))  #
+y2 = np.exp(-((x - 2) ** 2))
 fig, ax = plt.subplots()
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 
-ax.plot(x,y1, color="r", label="$f(x)= e^{-x^2}$")
-ax.plot(x,y2, color="b", label="$g(x) =e^{-(x-2)^2}$")
+ax.plot(x, y1, color="r", label="$f(x)= e^{-x^2}$")
+ax.plot(x, y2, color="b", label="$g(x) =e^{-(x-2)^2}$")
 ax.legend()
 ax.set_title("Plotting two functions in red and blue")
 ax.tick_params(labelsize=10)
 ax.grid("on")
 
 
-#%% Plot b
+# %% Plot b
 
 
 x0 = np.linspace(-2, 1, 100, endpoint=False)
-x1 = np.linspace(1+ 1e-14, 2, 100, endpoint = False)  #p.175
-x2 = np.linspace(2+ 1e-14, 5, 100)
+x1 = np.linspace(1 + 1e-14, 2, 100, endpoint=False)  # p.175
+x2 = np.linspace(2 + 1e-14, 5, 100)
 
 
 nominator = lambda x: x**2 - x + 1
-denominator = lambda x: (x-1)*(x-2)
+denominator = lambda x: (x - 1) * (x - 2)
 f = lambda x: nominator(x) / denominator(x)
 fig, ax = plt.subplots()
 
-ax.plot(x0,f(x0), color="b", label="$f(x) = \\frac{x^2 - x + 1}{(x-1)(x-2)}$")
-ax.plot(x1,f(x1), color="b")
-ax.plot(x2,f(x2), color="b")
+ax.plot(x0, f(x0), color="b", label="$f(x) = \\frac{x^2 - x + 1}{(x-1)(x-2)}$")
+ax.plot(x1, f(x1), color="b")
+ax.plot(x2, f(x2), color="b")
 
 ax.set_xlabel("x")
 ax.set_ylabel("y")
-ax.set_xlim([-2,5])
-ax.set_ylim([-20,20])
+ax.set_xlim([-2, 5])
+ax.set_ylim([-20, 20])
 ax.legend()
-ax.annotate('undefined x values', xy=(1, 15), xytext=(1, -4.8), arrowprops=dict(arrowstyle="->", color="k"))
-ax.annotate('', xy=(2.1, 15), xytext=(2, -3),
-            arrowprops=dict(arrowstyle='->', color='k'))
+ax.annotate(
+    "undefined x values",
+    xy=(1, 15),
+    xytext=(1, -4.8),
+    arrowprops=dict(arrowstyle="->", color="k"),
+)
+ax.annotate(
+    "",
+    xy=(2.1, 15),
+    xytext=(2, -3),
+    arrowprops=dict(arrowstyle="->", color="k"),
+)
 ax.set_title("Plotting a rational function")
 ax.tick_params(labelsize=10)
 ax.grid("on")
---
+++ A1/plot3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 23 00:57:32 2026
+
+@author: jkluge
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 6, 100)
+f = lambda t: 202 * np.exp(-0.42 * t)
+
+t_discrete = np.arange(0, 7)
+recorded = (205, 130, 85, 65, 42, 25, 15)
+
+
+fig, ax = plt.subplots()
+ax.plot(t, f(t), label="The measured data points")
+ax.plot(t_discrete, recorded, label="$A(t) = 202e^{-0.42t}$")
+ax.hlines(y=101, xmin=1.4, xmax=1.8)
+ax.annotate(
+    "Half-Life at t=1.636y",
+    xy=(1.65, 103),
+    xytext=(2, 150),
+    arrowprops=dict(arrowstyle="->", color="k"),
+)
+ax.set_xlabel("Time[y]")
+ax.set_ylabel("Radioactivity [kBq]")
+ax.grid("on")
+ax.legend()
+
+index = np.where(np.isclose(f(t), 101, 1e-2))
+print(
+    "At t = ",
+    t[index][0],
+    "years the radioactivity halfs to",
+    f(t)[index][0],
+    ".",
+)
